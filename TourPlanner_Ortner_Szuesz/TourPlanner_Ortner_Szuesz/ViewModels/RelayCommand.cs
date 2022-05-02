286f3efa3df490ac38063ea27f9da236383f658a
SaschaOr <26334597+SaschaOr@users.noreply.github.com>
2022-05-02 19:55:26 +0200

    ViewModel base and relay command

---
+++ TourPlanner_Ortner_Szuesz/TourPlanner_Ortner_Szuesz/ViewModels/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TourPlanner_Ortner_Szuesz.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _ExecuteAction;
+        private readonly Predicate<object> _CanExecutePredicate;
+
+        public void Execute(object? parameter)
+        {
+            _ExecuteAction(parameter);
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _CanExecutePredicate == null ? true : _CanExecutePredicate(parameter);
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if(execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            _ExecuteAction = execute;
+            _CanExecutePredicate = canExecute;
+        }
+    }
+}
